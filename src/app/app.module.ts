2cacda7ad06e7fc3985880f3082096d83321425d
Qt1337 <d.huh@t-online.de>
2020-11-07 10:38:34 +0100

    :bug: moved particles into background
    - added some css for app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgParticlesModule } from 'ng-particles';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageComponent],
   imports: [BrowserModule, NgParticlesModule],
   providers: [],
   bootstrap: [AppComponent]
