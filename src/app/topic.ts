fd7ea29b804b8f7e2af47a1c3601b1b1ff42e053
Qt1337 <d.huh@t-online.de>
2020-11-10 10:13:01 +0100

    :memo: updated topics

--- src/app/topic.ts
+++ src/app/topic.ts
@@ -2,5 +2,7 @@ export interface Topic {
   id: number;
   name: string;
   container_left: string[];
+  container_left_title: string;
   container_right: string[];
+  container_right_title: string;
 }
--- src/app/topics.ts
+++ src/app/topics.ts
@@ -1,9 +1,44 @@
 import {Topic} from './topic';
 
 export const TOPICS: Topic[] = [
-  {id: 0, name: 'start', container_left: ['test', 'test'], container_right: ['']},
-  {id: 1, name: 'angular cli', container_left: ['test', 'test'], container_right: ['']},
-  {id: 2, name: 'requirements', container_left: [''], container_right: ['']},
-  {id: 3, name: 'why angular', container_left: [''], container_right: ['']},
-  {id: 4, name: 'examples', container_left: [''], container_right: ['']},
+  {
+    id: 0,
+    name: 'start',
+    container_left: ['What is Angular CLI?', 'Why should you use Angular?', 'What do you need for Angular?', 'Who uses Angular?'],
+    container_left_title: 'Agenda',
+    container_right: [''],
+    container_right_title: ''
+  },
+  {
+    id: 1,
+    name: 'angular cli',
+    container_left: ['Frontend Web-Framework', 'written with TypeScript', 'Open-Source', 'not AngularJS'],
+    container_left_title: 'What is Angular CLI?',
+    container_right: ['Maintainer: Google LLC', 'Developer: Google LLC & Community', 'Release: 2016', 'Current Version: 10.2.2'],
+    container_right_title: 'Some Facts'
+  },
+  {
+    id: 2,
+    name: 'why angular?',
+    container_left: ['Platform independent', 'High Performance', 'Component-based architecture', 'TypeScript'],
+    container_left_title: 'PRO',
+    container_right: ['Declining number of developers (2018: 2nd Place 🡒 2019: 9th Place)', 'Migrating takes time', 'Complexity'],
+    container_right_title: 'CON'
+  },
+  {
+    id: 3,
+    name: 'examples',
+    container_left: ['Google Apps (Gmail and so on)', 'Microsoft Office', 'Deutsche Bank', 'PayPal'],
+    container_left_title: 'Who uses Angular?',
+    container_right: [''],
+    container_right_title: ''
+  },
+  {
+    id: 4,
+    name: 'requirements',
+    container_left: ['JavaScript', 'HTML', 'CSS'],
+    container_left_title: 'What should you know?',
+    container_right: ['Node.js', 'npm package manager', '"npm install -g @angular/cli"'],
+    container_right_title: 'What you need to install:'
+  },
 ];
